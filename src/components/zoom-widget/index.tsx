8e8eee18cf770991fed7b9c620f0d440fc1c9f81
unknown <gianni.infi@gmail.com>
2023-02-26 01:51:03 -0500

    added disabled styling to toolbar-button, disabling zoom buttons on min and max zoom

--- src/components/zoom-widget/index.tsx
+++ src/components/zoom-widget/index.tsx
@@ -26,13 +26,13 @@ const ZoomWidget: FunctionComponent<ZoomWidgetProps> = ({ onChangeZoom, currentZ
 
     return (
         <WidgetContainer className='absolute top-2 right-2' spacingClassName='flex-col space-y-2' inOverlayState={inOverlayState}>
-            <ToolbarButton onClick={handleZoomIn}>
+            <ToolbarButton onClick={handleZoomIn} disabled={currentZoom === MAX_ZOOM}>
                 <div className='flex'>
                     <ZoomInIcon className="w-6 h-6" />
                 </div>
             </ToolbarButton>
             <div className="flex justify-center font-semibold text-sm">{currentZoom % 1 != 0 ? currentZoom.toFixed(2) : currentZoom}x</div>
-            <ToolbarButton onClick={handleZoomOut}>
+            <ToolbarButton onClick={handleZoomOut} disabled={currentZoom === MIN_ZOOM}>
                 <div className='flex'>
                     <ZoomOutIcon className="w-6 h-6" />
                 </div>
